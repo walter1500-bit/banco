15e723333ca017944a34ac51082ec9f4c47a3d95
walter1500-bit <walter1500@gmail.com>
2026-03-23 15:52:14 -0600

    Deploy

--- Banco/settings.py
+++ Banco/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mnil6b^&=l4u$sx_bz6k5ut*d*)tn0!dgslsf6wh#3wu-@jp&x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
